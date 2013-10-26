035ff7652acfb461cb2cc152bcc2055a2f4556b6
Jorge Escalante <jorge.escalante.h@gmail.com>
2013-10-26 15:29:55 -0600

    risas

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Heroku. I take the control now and forever! You are mine now');
+  response.send('Hello Heroku. I take the control now and forever! You are mine now. HAHAHAHA');
 });
 
 var port = process.env.PORT || 5000;
