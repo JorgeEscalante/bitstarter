d6ef1306782ecad7672c8ac4f87f0f75f67cc0a9
Jorge Escalante <jorge.escalante.h@gmail.com>
2013-11-01 00:27:26 -0600

    trying to read the file

--- web.js
+++ web.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+fs.writeFile('index.html', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
@@ -10,3 +15,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
